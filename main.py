3a2a139ae86e28a5adf7afbc54e7bb78bce36ec1
nlevy48 <155464271+nlevy48@users.noreply.github.com>
2024-04-11 15:31:17 -0400

    3:30 4/11/24 moving paddles don't work

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from turtle import Screen
-import ball
-import paddle
+from ball import Ball #when importing a class need to do from ____ import ___ and then the class
+from paddle import Paddle
 
 left_paddle_location = (-280, 0)
 
 right_paddle_location = (280, 0)
 
+UP = 90
+DOWN = 270
+
 s = Screen()
+
+s.tracer(0)
+
 
 s.bgcolor("black")
 
-s.setup(width= 500, height= 500)
+s.setup(width= 600, height= 600)
 
-
-
-paddle_1 = Paddle()
-
-
-
+paddle_1 = Paddle(left_paddle_location)
+paddle_2 = Paddle(right_paddle_location)
 
 
 
@@ -25,5 +27,22 @@ paddle_1 = Paddle()
 
 
 
+s.onkeypress(paddle_1.up, key= "w") #onkeypress() allows continual pressing
+s.onkeypress(paddle_1.down, key= "s")
+s.onkeypress(paddle_2.up, key= "Up")
+s.onkeypress(paddle_2.down, key= "Down")
 
+s.listen()
+
+
+
+
+
+
+
+
+
+
+
+s.update()
 s.exitonclick()
--- paddle.py
+++ paddle.py
@@ -5,26 +5,27 @@ DOWN = 270
 
 class Paddle(Turtle):
 
-    def __init__(self, starting_location,):
-        self.Paddle_Side
-
+    def __init__(self, starting_location):
         super().__init__()
         self = Turtle("square")
         self.color("white")
+        self.penup()
         self.goto(starting_location)
-        self.shapesize(10, 100)
-
+        self.shapesize(15, 2)
 
     def up(self):
-        if self.paddle.heading() != UP:
+        if self.heading() != UP:
             self.setheading(UP)
             self.forward(20)
+        
 
 
     def down(self):
         if self.heading() != DOWN:
             self.setheading(DOWN)
             self.forward(20)
+        
+
 
     
 
